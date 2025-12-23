29e1abf77c4f582400a098479d857c2ba17a78a0
Tomilola662 <tomilola243@gmail.com>
2025-12-23 21:34:06 +0100

    feat: worked on the text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Text } from './Text/Text';
 
-const App: React.FC = () => <div>Welcome</div>;
+const App: React.FC = () => (
+  <div>
+    Welcome <Text fontSize="fs-2xl">Testing text</Text>
+  </div>
+);
 
 export default App;
