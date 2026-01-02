9046e69915e707df67500b08a022c61ca5e69dc5
mosidrum <mosiokanga@gmail.com>
2026-01-02 06:04:38 +0100

    feat: add target section

---
+++ src/components/Target/Target.tsx
@@ -0,0 +1,35 @@
+import { HeroText, MediumText } from '../Text';
+import happyGirl from '../../../public/happy-girl.png';
+import styles from './Target.module.scss';
+import { PageContainer } from '../PageContainer';
+
+const items = [
+  'Students and Graduates exploring career options',
+  'Civil servants seeking side income or late career - pivots.',
+  'Mid - career professionals transitioning into tech.',
+  'Business owners exploring digital skills to grow ventures',
+  'Job seekers looking for skills in demand locally and globally.',
+];
+
+export const Target = () => (
+  <PageContainer>
+    <div className="text-center py-13">
+      <HeroText variant="black" weight="bold">
+        Targets
+      </HeroText>
+      <MediumText weight="medium">
+        Built for individuals seeking clear, data-driven guidance into tech or business careers.
+      </MediumText>
+    </div>
+    <div className={styles.grid}>
+      {items.map((item) => (
+        <div className={styles.target} key={item}>
+          <img src={happyGirl} alt="Happy girl" className={styles.image} />
+          <MediumText className="text-center" variant="secondary">
+            {item}
+          </MediumText>
+        </div>
+      ))}
+    </div>
+  </PageContainer>
+);
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export * from './Button';
 export * from './HowItWorks';
 export * from './AboutUs';
 export * from './PageContainer';
+export * from './Target';
